d27ef1c3a0a666c564596f109ad69512c7d9f41b
Christian Helmich <kagekirin@gmail.com>
2012-02-18 18:35:13 +0900

    regenerated blog

---
+++ regen.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "regenerating site"
+git add _config.yml
+git commit -m "updated _config.yml"
+git add sass
+git commit -m "updated stylesheets"
+git add source
+git commit -m "updated blog pages"
+rake generate
+git add . 
+git commit -am "regenerated blog"
+git push heroku master
